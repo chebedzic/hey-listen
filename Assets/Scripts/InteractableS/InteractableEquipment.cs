4ac63c74f1d447aed6dfc2aff5b3194193fd5c3d
André Cardoso <amc.acardoso@gmail.com>
2023-07-24 23:08:08 +0200

    tried fixing gap issue, turning off MSAA

--- Assets/Scripts/InteractableS/InteractableEquipment.cs
+++ Assets/Scripts/InteractableS/InteractableEquipment.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class InteractableEquipment : Interactable
 {
-
+    public bool bubble = true;
     public Equipment equipment;
 
     public override void OnMouseEnter()
     {
+        if (!bubble) return;
         base.OnMouseEnter();
         CompanionManager.instance.currentEquipmentBubble = this;
     }
 
     public override void OnMouseExit()
     {
+        if (!bubble) return;
         base.OnMouseExit();
         CompanionManager.instance.currentEquipmentBubble = null;
 
@@ -23,6 +25,7 @@ public class InteractableEquipment : Interactable
 
     public override void OnMouseDown()
     {
+        if (!bubble) return;
         base.OnMouseDown();
 
         if(equipment == null) { print("Equipment Object Null"); }
@@ -30,8 +33,20 @@ public class InteractableEquipment : Interactable
         EquipmentManager.instance.ShowEquipments(false);
     }
 
-    public void GotShield()
+    private void OnTriggerEnter(Collider other)
     {
-        GameManager.instance.hasUnlockedShield= true;
+        if (bubble)
+            return;
+
+        if (other.CompareTag("Hero"))
+        {
+            if (equipment == null) { print("Equipment Object Null"); }
+            HeroManager.instance.SetHeroDestination(HeroManager.instance.transform.position);
+            HeroManager.instance.SetHeroEquipment(equipment);
+            EquipmentManager.instance.ShowEquipments(false);
+            GameManager.instance.hasUnlockedShield = true;
+            gameObject.SetActive(false);
+        }
+        
     }
 }
